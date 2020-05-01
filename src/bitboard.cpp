110440e9084e29c3114089f5b6a8a32a56e5d8cb
Toshihiko Okuhara <okuhara@amy.hi-ho.ne.jp>
2020-05-01 16:14:15 +0900

    Experimental AVX2 computational (non-magic) sliding attacks
    Trial of computational sliding attacks without magic bitboard
    using AVX2 intrinsics.  Better than chessprogramming wiki's
    Dumb7Fill AVX2 example, still 3% slower on Haswell than BMI2(pext)
    build while it requires same CPU as BMI2.  So basically useless,
    only for interest.
    
    AVX512CD code is tested on emulator only.
    
    No functional change.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -35,6 +35,13 @@ Bitboard PawnAttacks[COLOR_NB][SQUARE_NB];
 Magic RookMagics[SQUARE_NB];
 Magic BishopMagics[SQUARE_NB];
 
+#ifdef __AVX2__
+__m256i queen_mask_v4[SQUARE_NB][2];
+__m128i rook_mask_NS[SQUARE_NB];
+uint8_t rook_attacks_EW[64 * 8];
+__m256i bishop_mask_v4[SQUARE_NB];
+#endif
+
 namespace {
 
   Bitboard RookTable[0x19000];  // To store rook attacks
@@ -83,6 +90,54 @@ void Bitboards::init() {
 
   init_magics(RookTable, RookMagics, RookDirections);
   init_magics(BishopTable, BishopMagics, BishopDirections);
+
+#ifdef __AVX2__
+
+  Direction vecDirections[2][4] = {{ EAST, NORTH, NORTH_EAST, NORTH_WEST }, { WEST, SOUTH, SOUTH_WEST, SOUTH_EAST }};
+  Bitboard vecAttacks[4];
+
+  // pseudo attacks for Queen 8 directions
+  for (Square sq = SQ_A1; sq <= SQ_H8; ++sq)
+    for (int j = 0; j < 2; ++j) {
+      for (int i = 0; i < 4; ++i) {
+        vecAttacks[i] = 0;
+        Square s = sq;
+        while(safe_destination(s, vecDirections[j][i]))
+            vecAttacks[i] |= (s += vecDirections[j][i]);
+      }
+      queen_mask_v4[sq][j] = _mm256_set_epi64x(vecAttacks[3], vecAttacks[2], vecAttacks[1], vecAttacks[0]);
+    }
+
+  // pseudo attacks for Rook (NORTH-SOUTH) and Bishop
+  for (Square sq = SQ_A1; sq <= SQ_H8; ++sq) {
+    rook_mask_NS[sq] = _mm_set_epi64x(
+      _mm256_extract_epi64(queen_mask_v4[flip_rank(sq)][0], 1),	// SOUTH (vertically flipped)
+      _mm256_extract_epi64(queen_mask_v4[sq][0], 1));	// NORTH
+    bishop_mask_v4[sq] = _mm256_set_epi64x(
+      _mm256_extract_epi64(queen_mask_v4[flip_rank(sq)][0], 2),	// SOUTH_EAST (vertically flipped)
+      _mm256_extract_epi64(queen_mask_v4[sq][0], 3),	// NORTH_WEST
+      _mm256_extract_epi64(queen_mask_v4[flip_rank(sq)][0], 3),	// SOUTH_WEST (vertically flipped)
+      _mm256_extract_epi64(queen_mask_v4[sq][0], 2));	// NORTH_EAST
+  }
+
+  // sliding attacks for Rook EAST-WEST
+  for (int occ = 0; occ < 128; occ += 2)	// inner 6 bits
+    for (int sq = 0; sq < 8; ++sq) {
+      uint8_t attack = 0;
+      for (uint8_t s = (1 << sq) << 1; s; s <<= 1) {
+	attack |= s;
+        if (occ & s)
+          break;
+      }
+      for (uint8_t s = (1 << sq) >> 1; s; s >>= 1) {
+	attack |= s;
+        if (occ & s)
+          break;
+      }
+      rook_attacks_EW[occ * 4 + sq] = attack;
+    }
+
+#endif
 
   for (Square s1 = SQ_A1; s1 <= SQ_H8; ++s1)
   {
--- src/bitboard.h
+++ src/bitboard.h
@@ -271,10 +271,115 @@ inline Bitboard safe_destination(Square s, int step)
 
 template<PieceType Pt>
 inline Bitboard attacks_bb(Square s, Bitboard occupied) {
+  if (Pt == QUEEN)
+    return attacks_bb<BISHOP>(s, occupied) | attacks_bb<ROOK>(s, occupied);
 
   const Magic& m = Pt == ROOK ? RookMagics[s] : BishopMagics[s];
   return m.attacks[m.index(occupied)];
 }
+
+#ifdef __AVX2__
+/// AVX2 (non-magic) bitboard
+
+extern __m256i queen_mask_v4[SQUARE_NB][2];
+extern __m256i bishop_mask_v4[SQUARE_NB];
+extern __m128i rook_mask_NS[SQUARE_NB];
+extern uint8_t rook_attacks_EW[64 * 8];
+
+// avx2/sse2 versions of BLSMSK (https://www.chessprogramming.org/BMI1#BLSMSK)
+inline __m256i blsmsk64x4(__m256i y) {
+  return _mm256_xor_si256(_mm256_add_epi64(y, _mm256_set1_epi64x(-1)), y);
+}
+inline __m128i blsmsk64x2(__m128i x) {
+  return _mm_xor_si128(_mm_add_epi64(x, _mm_set1_epi64x(-1)), x);
+}
+
+template<> inline Bitboard attacks_bb<QUEEN>(Square s, Bitboard occupied)
+{
+  const __m256i occupied4 = _mm256_set1_epi64x(occupied);
+  const __m256i lmask = queen_mask_v4[s][0];
+  const __m256i rmask = queen_mask_v4[s][1];
+
+    // Left bits: set mask bits lower than occupied LS1B
+  __m256i slide4 = _mm256_and_si256(occupied4, lmask);
+#if defined(__AVX512CD__) && defined(__AVX512VL__)
+  // slide4 = _mm256_and_si256(blsmsk64x4(slide4), lmask);
+  slide4 = _mm256_ternarylogic_epi64(slide4, _mm256_add_epi64(slide4, _mm256_set1_epi64x(-1)), lmask, 0x28);
+    // Right bits: set mask bits higher than occupied MS1B
+  __m256i rslide = _mm256_srav_epi64(_mm256_set1_epi64x(0x8000000000000000),
+    _mm256_lzcnt_epi64(_mm256_and_si256(occupied4, rmask)));
+  // slide4 = _mm256_or_si256(slide4, _mm256_and_si256(rslide, rmask));
+  slide4 = _mm256_ternarylogic_epi64(slide4, rslide, rmask, 0xf8);
+
+#else
+  slide4 = _mm256_and_si256(blsmsk64x4(slide4), lmask);
+    // Right bits: set shadow bits lower than occupied MS1B (6 bits max)
+  __m256i shadow = _mm256_srlv_epi64(_mm256_and_si256(occupied4, rmask), _mm256_set_epi64x(7, 9, 8, 1));
+  shadow = _mm256_or_si256(shadow, _mm256_srlv_epi64(shadow, _mm256_set_epi64x(7, 9, 8, 1)));  // PP Fill
+  shadow = _mm256_or_si256(shadow, _mm256_srlv_epi64(shadow, _mm256_set_epi64x(14, 18, 16, 2)));
+  shadow = _mm256_or_si256(shadow, _mm256_srlv_epi64(shadow, _mm256_set_epi64x(14, 18, 16, 2)));
+    // add mask bits higher than blocker
+  slide4 = _mm256_or_si256(slide4, _mm256_andnot_si256(shadow, rmask));
+#endif
+
+    // OR 4 vectors
+  __m128i slide2 = _mm_or_si128(_mm256_castsi256_si128(slide4), _mm256_extracti128_si256(slide4, 1));
+  return _mm_cvtsi128_si64(_mm_or_si128(slide2, _mm_unpackhi_epi64(slide2, slide2)));
+}
+
+template<> inline Bitboard attacks_bb<ROOK>(Square s, Bitboard occupied)
+{
+#if defined(__AVX512CD__) && defined(__AVX512VL__)
+  const __m128i occupied2 = _mm_set1_epi64x(occupied);
+  const __m128i lmask = _mm256_castsi256_si128(queen_mask_v4[s][0]);
+  const __m128i rmask = _mm256_castsi256_si128(queen_mask_v4[s][1]);
+
+    // Left bits: set mask bits lower than occupied LS1B
+  __m128i slide2 = _mm_and_si128(occupied2, lmask);
+  // slide2 = _mm_and_si128(blsmsk64x2(slide2), mask);
+  slide2 = _mm_ternarylogic_epi64(slide2, _mm_add_epi64(slide2, _mm_set1_epi64x(-1)), lmask, 0x28);
+    // Right bits: set mask bits higher than occupied MS1B
+  __m128i rslide = _mm_srav_epi64(_mm_set1_epi64x(0x8000000000000000),
+    _mm_lzcnt_epi64(_mm_and_si128(occupied2, rmask)));
+  // slide2 = _mm_or_si128(slide2, _mm_and_si128(rslide, rmask));
+  slide2 = _mm_ternarylogic_epi64(slide2, rslide, rmask, 0xf8);
+
+  return _mm_cvtsi128_si64(_mm_or_si128(slide2, _mm_unpackhi_epi64(slide2, slide2)));
+
+#else
+    // South: flip vertical to simulate MS1B by LS1B
+  const __m128i swapl2h = _mm_set_epi8(0, 1, 2, 3, 4, 5, 6, 7, 7, 6, 5, 4, 3, 2, 1, 0);
+  __m128i occupied2 = _mm_shuffle_epi8(_mm_cvtsi64_si128(occupied), swapl2h);
+  const __m128i mask = rook_mask_NS[s];
+    // North: set mask bits lower than occupied LS1B
+  __m128i slide2 = _mm_and_si128(blsmsk64x2(_mm_and_si128(occupied2, mask)), mask);
+  const __m128i swaph2l = _mm_set_epi8(15, 14, 13, 12, 11, 10, 9, 8, 8, 9, 10, 11, 12, 13, 14, 15);
+  Bitboard slides = _mm_cvtsi128_si64(_mm_or_si128(slide2, _mm_shuffle_epi8(slide2, swaph2l)));
+
+    // East-West: from precomputed table
+  int r8 = rank_of(s) * 8;
+  slides |= Bitboard(rook_attacks_EW[((occupied >> r8) & 0x7e) * 4 + file_of(s)]) << r8;
+  return slides;
+#endif
+}
+
+template<> inline Bitboard attacks_bb<BISHOP>(Square s, Bitboard occupied)
+{
+    // flip vertical to simulate MS1B by LS1B
+  const __m128i swapl2h = _mm_set_epi8(0, 1, 2, 3, 4, 5, 6, 7, 7, 6, 5, 4, 3, 2, 1, 0);
+  __m128i occupied2 = _mm_shuffle_epi8(_mm_cvtsi64_si128(occupied), swapl2h);
+  __m256i occupied4 = _mm256_broadcastsi128_si256(occupied2);
+
+  const __m256i mask = bishop_mask_v4[s];
+    // set mask bits lower than occupied LS1B
+  __m256i slide4 = _mm256_and_si256(blsmsk64x4(_mm256_and_si256(occupied4, mask)), mask);
+
+  __m128i slide2 = _mm_or_si128(_mm256_castsi256_si128(slide4), _mm256_extracti128_si256(slide4, 1));
+  const __m128i swaph2l = _mm_set_epi8(15, 14, 13, 12, 11, 10, 9, 8, 8, 9, 10, 11, 12, 13, 14, 15);
+  return _mm_cvtsi128_si64(_mm_or_si128(slide2, _mm_shuffle_epi8(slide2, swaph2l)));
+}
+
+#endif // __AVX2__
 
 inline Bitboard attacks_bb(PieceType pt, Square s, Bitboard occupied) {
 
@@ -284,7 +389,7 @@ inline Bitboard attacks_bb(PieceType pt, Square s, Bitboard occupied) {
   {
   case BISHOP: return attacks_bb<BISHOP>(s, occupied);
   case ROOK  : return attacks_bb<  ROOK>(s, occupied);
-  case QUEEN : return attacks_bb<BISHOP>(s, occupied) | attacks_bb<ROOK>(s, occupied);
+  case QUEEN : return attacks_bb< QUEEN>(s, occupied);
   default    : return PseudoAttacks[pt][s];
   }
 }
